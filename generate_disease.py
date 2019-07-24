55b3f46996545fbfaa9efbc26e481edaea22ffd7
Sudeep Raj <36677071+rajsudeep@users.noreply.github.com>
2019-07-23 22:22:44 -0700

    Rollback to version 1

--- generate_disease.py
+++ generate_disease.py
@@ -2,24 +2,36 @@ from random import choice, randint
 import constants as c
 
 
-def adjective():
-    return '{}'.format(
-        choice(c.ADJECTIVES)
+def adj_organ():
+    return '{} {}'.format(
+        choice(c.ADJECTIVES),
+        choice(c.BODY_PARTS)
     )
 
 
-def color():
-    return '{}'.format(
-        choice(c.COLORS)
+def color_bile():
+    return 'an excess of {} bile'.format(
+        choice(c.COLORS).lower()
     )
 
 
-def superlative():
-    return '{}'.format(
-        choice(c.SUPERLATIVES)
+def superlative_symptom():
+    return '{} {}'.format(
+        choice(c.SUPERLATIVES),
+        choice(c.SYMPTOMS)
     )
 
-def noun():
-    return '{}'.format(
-        choice(c.NOUNS)
-)
+
+def disease():
+    index = randint(0, 5)
+    if index == 0:
+        return color_bile()
+    elif index == 1:
+        return superlative_symptom()
+    else:
+        return adj_organ()
+
+
+# for testing
+if __name__ == '__main__':
+    print(disease())
